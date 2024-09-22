d3bb4c0f6fb8f423305148d820a14d5c4e4f3cf0
Bibek Ghimire <bibekghimire99@gmail.com>
2024-09-22 06:51:27 +0545

    nothing

--- library/settings.py
+++ library/settings.py
@@ -23,7 +23,9 @@ load_dotenv(env_path)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
